fa603286833f1c8a5b18ce72cc14aec39e2401ec
mjesiolowski <maciej.jesiolowski@gmail.com>
2019-03-22 17:35:53 +0100

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+var sass = require('gulp-sass');
+
+gulp.task('sass', function () {
+   return gulp.src('src/scss/*.scss')
+      .pipe(sass())
+      .pipe(gulp.dest("src/css"))
+      .pipe(browserSync.stream());
+});
+
+gulp.task('serve', gulp.series('sass', function () {
+
+   browserSync.init({
+      server: "./src"
+   });
+
+   gulp.watch('src/scss/*.scss', gulp.series('sass'));
+   gulp.watch("src/*.html").on('change', browserSync.reload);
+   gulp.watch("src/*.js").on('change', browserSync.reload);
+}));
+
+gulp.task("default", (gulp.series("serve")));

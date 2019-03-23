c758de6ca145b9297132970f93f85f06e1797d09
mjesiolowski <maciej.jesiolowski@gmail.com>
2019-03-23 22:40:19 +0100

    about and contact sections add

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,8 @@
 const navbar = document.querySelector('.menu')
+const catolog = document.querySelector('.catalog')
+const about = document.querySelector('.about')
+const contact = document.querySelector('.contact')
+const navbarLinks = document.querySelectorAll('.menu__link')
 const catalogSection = document.querySelectorAll('.catalog__section')
 
 
@@ -7,4 +11,20 @@ window.addEventListener("scroll", function () {
       navbar.style.fontSize = "1.6rem"
       catalogSection.forEach(section => section.style.left = 0)
    } else navbar.style.fontSize = "2.5rem"
+
+   navbarLinks.forEach(navbarLink => navbarLink.classList.remove('menu__link__active'))
+
+   console.log(window.scrollY, contact.offsetTop)
+
+   if (window.scrollY >= catalog.offsetTop && window.scrollY < about.offsetTop) {
+      navbarLinks[0].classList.add('menu__link__active')
+   }
+   if (window.scrollY >= about.offsetTop && window.scrollY < contact.offsetTop - (contact.offsetTop / 10)) {
+      navbarLinks[1].classList.add('menu__link__active')
+   }
+   if (window.scrollY >= contact.offsetTop - (contact.offsetTop / 10)) {
+      navbarLinks[2].classList.add('menu__link__active')
+   }
+
+
 })

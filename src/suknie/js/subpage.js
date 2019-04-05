2dbe77222e9e152f50df7b53a3d54ef28fd70948
mjesiolowski <maciej.jesiolowski@gmail.com>
2019-04-05 12:30:33 +0200

    add working modal

--- src/suknie/js/subpage.js
+++ src/suknie/js/subpage.js
@@ -1,14 +1,19 @@
 const navbar = document.querySelector('.menu')
-
-const catalogSection = document.querySelectorAll('.catalog__section')
-const catalogSectionSubpage = document.querySelectorAll('.catalog__section--subpage')
-const modal = document.querySelector('.catalog__modal')
-const modalImage = document.querySelector('.catalog__modal__img')
-const modalCloseBtn = document.querySelector('.catalog__modal__closeBtn')
+const navbarLinks = document.querySelectorAll('.menu__link')
+const dropdownList = document.querySelector('.menu__dropdown__list')
 
 const hamburger = document.querySelector('.menu__burger')
 const hiddenMenu = document.querySelector('.menu__hidden')
 const hiddenMenuLinks = document.querySelectorAll('.menu__hidden__link')
+
+const catalogSection = document.querySelectorAll('.catalog__section')
+const catalogSectionSubpageImages = document.querySelectorAll('.catalog__section--subpage__img')
+
+const modal = document.querySelector('.catalog__modal')
+const modalImage = document.querySelector('.catalog__modal__img')
+const modalCloseBtn = document.querySelector('.catalog__modal__closeBtn')
+const modalRightArrow = document.querySelector('.catalog__modal__rightArrow')
+const modalLeftArrow = document.querySelector('.catalog__modal__leftArrow')
 
 //scroll handlers
 window.addEventListener("scroll", function () {
@@ -29,15 +34,37 @@ hiddenMenuLinks.forEach(link => link.addEventListener('click', () => {
 }))
 
 //modal handlers
-catalogSectionSubpage.forEach(section => {
-   section.addEventListener('click', (e) => {
+catalogSectionSubpageImages.forEach((section, index) => {
+   section.addEventListener('click', () => {
       modal.style.visibility = "visible";
-      const srcValue = e.target.attributes.src.nodeValue
+      let indexValue = index;
+      let srcValue = catalogSectionSubpageImages[index].attributes.src.nodeValue
       modalImage.setAttribute('src', srcValue)
 
+      modalRightArrow.addEventListener('click', () => {
+         if (indexValue === catalogSectionSubpageImages.length - 1) {
+            indexValue = -1
+         }
+         srcValue = catalogSectionSubpageImages[++indexValue].attributes.src.nodeValue
+         modalImage.setAttribute('src', srcValue)
+      })
+
+      modalLeftArrow.addEventListener('click', () => {
+         if (indexValue === 0) {
+            indexValue = catalogSectionSubpageImages.length
+         }
+         srcValue = catalogSectionSubpageImages[--indexValue].attributes.src.nodeValue
+         modalImage.setAttribute('src', srcValue)
+      })
    })
 })
 
 modalCloseBtn.addEventListener('click', () => {
    modal.style.visibility = "hidden"
+})
+
+//dropdown handlers
+
+navbarLinks[1].addEventListener('click', () => {
+   dropdownList.classList.toggle('menu__dropdown__list--active')
 })

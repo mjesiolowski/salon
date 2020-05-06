5e42a47b37703e02da0a5dbbc0ae96fa65eff925
mjesiolowski <maciej.jesiolowski@gmail.com>
2020-05-06 12:08:01 +0200

    added lazy loading, simplified modals

--- src/scripts/subpage.js
+++ src/scripts/subpage.js
@@ -51,6 +51,8 @@ const subpage = {
       this.modalRightArrow = document.querySelector('.catalog__modal__rightArrow')
       this.modalLeftArrow = document.querySelector('.catalog__modal__leftArrow')
 
+      // this.subpageImages = []
+
       this.footerDate = document.querySelector('.footer__date')
 
       // this.images.forEach(({ name, number }) => {
@@ -76,11 +78,13 @@ const subpage = {
          this.hamburger.classList.toggle('menu__burger--active')
       })
 
+      // this.handleModal(this.subpageImages)
+
       this.modal.addEventListener('click', (e) => {
          if (e.target.id === 'catalog_modal') this.hideModal()
       })
 
-      this.modalCloseBtn.addEventListener('click', () => this.hideModal())
+      // this.modalCloseBtn.addEventListener('click', () => this.hideModal())
 
       document.addEventListener('click', (e) => {
          if (e.target.id === 'menu-link')
@@ -88,80 +92,145 @@ const subpage = {
          else this.dropdownList.classList.remove('menu__dropdown__list--active')
       })
 
+      document.addEventListener("DOMContentLoaded", function () {
+         let lazyloadImages;
+
+         if ("IntersectionObserver" in window) {
+            lazyloadImages = document.querySelectorAll(".lazy");
+            const imageObserver = new IntersectionObserver(function (entries, observer) {
+               entries.forEach(function (entry) {
+                  if (entry.isIntersecting) {
+                     const image = entry.target;
+                     image.src = image.dataset.src;
+                     image.classList.remove("lazy");
+                     imageObserver.unobserve(image);
+                  }
+               });
+            });
+
+            lazyloadImages.forEach(function (image) {
+               imageObserver.observe(image);
+            });
+         } else {
+            let lazyloadThrottleTimeout;
+            lazyloadImages = document.querySelectorAll(".lazy");
+
+            function lazyload() {
+               if (lazyloadThrottleTimeout) {
+                  clearTimeout(lazyloadThrottleTimeout);
+               }
+
+               lazyloadThrottleTimeout = setTimeout(function () {
+                  const scrollTop = window.pageYOffset;
+                  lazyloadImages.forEach(function (img) {
+                     if (img.offsetTop < (window.innerHeight + scrollTop)) {
+                        img.src = img.dataset.src;
+                        img.classList.remove('lazy');
+                     }
+                  });
+                  if (lazyloadImages.length == 0) {
+                     document.removeEventListener("scroll", lazyload);
+                     window.removeEventListener("resize", lazyload);
+                     window.removeEventListener("orientationChange", lazyload);
+                  }
+               }, 20);
+            }
+
+            document.addEventListener("scroll", lazyload);
+            window.addEventListener("resize", lazyload);
+            window.addEventListener("orientationChange", lazyload);
+         }
+      })
+
       this.setFooterDate()
       this.toggleHamburger()
-      this.generateImages()
+      this.generateDOM()
    },
    toggleHamburger() {
       this.hiddenMenuLinks.forEach(link => link.addEventListener('click', () => {
          this.hiddenMenu.classList.toggle('menu__hidden--active')
       }))
    },
-   handleModal(subpageImg) {
-      return subpageImg.forEach((img, index) => {
+   handleModal(subpageImages) {
+      subpageImages.forEach((img, index) => {
          img.addEventListener('click', () => {
-            this.modal.style.visibility = "visible";
-            let indexValue = index;
-            let srcValue = subpageImg[index].attributes.src.nodeValue
-
-            this.modalImage.setAttribute('src', srcValue)
-
             const handleRightArrowModal = () => {
-               if (indexValue === subpageImg.length - 1) {
+               console.log(indexValue)
+               if (indexValue === subpageImages.length - 1) {
                   indexValue = -1
                }
-               srcValue = subpageImg[++indexValue].attributes.src.nodeValue
+               srcValue = subpageImages[++indexValue].dataset.src
                this.modalImage.setAttribute('src', srcValue)
             }
 
-            const handleLeftArrowModal = () => {
+            const handleLeftArrowModal = (indexValue, subpageImages) => {
+               console.log(indexValue)
                if (indexValue === 0) {
-                  indexValue = subpageImg.length
+                  indexValue = subpageImages.length
                }
-               srcValue = subpageImg[--indexValue].attributes.src.nodeValue
+               srcValue = subpageImages[--indexValue].dataset.src
                this.modalImage.setAttribute('src', srcValue)
             }
+
+            this.modal.style.visibility = "visible";
+            let indexValue = index;
+            let srcValue = subpageImages[index].dataset.src
+            this.modalImage.setAttribute('src', srcValue)
 
             this.modalRightArrow.addEventListener('click', handleRightArrowModal)
 
-            this.modalLeftArrow.addEventListener('click', handleLeftArrowModal)
-
-            document.addEventListener('keydown', (e) => {
-               if (e.keyCode === 39) handleRightArrowModal()
-               else if (e.keyCode === 37) handleLeftArrowModal()
-            })
+            this.modalLeftArrow.addEventListener('click', () => handleLeftArrowModal(indexValue, subpageImages))
+            this.modalCloseBtn.addEventListener('click', () => this.hideModal())
+            this.modalCloseBtn.addEventListener('click', () => this.modalRightArrow.removeEventListener('click', handleRightArrowModal))
+            // this.modalCloseBtn.addEventListener('click', () => this.hideModal())
+            // this.modalCloseBtn.addEventListener('click', () => this.hideModal())
+            // document.addEventListener('keydown', (e) => {
+            //    if (e.keyCode === 39) handleRightArrowModal()
+            //    else if (e.keyCode === 37) handleLeftArrowModal()
+            // })
 
          })
       })
    },
    hideModal() {
       this.modal.style.visibility = "hidden"
+      this.modalRightArrow.removeEventListener('click', () => this.handleRightArrowModal(indexValue, subpageImages))
+      this.modalLeftArrow.removeEventListener('click', () => this.handleLeftArrowModal(indexValue, subpageImages))
    },
-   generateDOM(number, collection) {
+
+   generateCollectionImages(photosNumber, collection) {
       const location = window.location.pathname.split('/')[2]
 
       if (location === `${collection}.html`) {
-         const div = document.createElement('div')
-         div.classList.add(`catalog__section`)
-         div.classList.add(`catalog__section--subpage`)
-         catalog.appendChild(div)
+         for (let i = 1; i <= photosNumber; i++) {
+            const div = document.createElement('div')
+            const img = document.createElement('img')
 
-         const img = document.createElement('img')
-         img.classList.add('catalog__section--subpage__img')
-         img.setAttribute('alt', 'wedding dress photo')
-         img.setAttribute('src', `../images/${collection}/${collection} (${number++}).jpg`)
-         div.appendChild(img)
+            div.classList.add(`catalog__section`)
+            div.classList.add(`catalog__section--subpage`)
+            catalog.appendChild(div)
 
-         let subpageImg = document.querySelectorAll('.catalog__section--subpage__img')
+            img.classList.add('catalog__section--subpage__img')
+            img.classList.add('lazy')
+            img.setAttribute('alt', 'wedding dress photo')
+            img.setAttribute('data-src', `../images/${collection}/${collection} (${i}).jpg`)
+            div.appendChild(img)
 
-         this.handleModal(subpageImg)
+            if (i === photosNumber) {
+               let subpageImages = document.querySelectorAll('.catalog__section--subpage__img')
+               this.handleModal(subpageImages)
+            }
+         }
+
+         // console.log(this.subpageImages)
+         // let subpageImages = document.querySelectorAll('.catalog__section--subpage__img')
+
+         // this.handleModal(subpageImages)
       }
    },
-   generateImages() {
+   generateDOM() {
       this.images.forEach(({ collection, photosNumber }) => {
-         for (let i = 1; i <= photosNumber; i++) {
-            this.generateDOM.call(this, i, collection)
-         }
+         this.generateCollectionImages.call(this, photosNumber, collection)
       })
    },
    setFooterDate() {
